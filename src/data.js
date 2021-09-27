08eef6ce20e1e997f127dd63ceed3de5897ef9bd
Signal Fish <signalfish38861721@gmail.com>
2021-09-27 10:14:17 -0400

    bug fixed

--- src/data.js
+++ src/data.js
@@ -1,27 +1,27 @@
 export const links = [
   {
     id: 1,
-    url: "/",
+    url: "#",
     text: "home",
   },
   {
     id: 2,
-    url: "/about",
+    url: "#",
     text: "about",
   },
   {
     id: 3,
-    url: "/projects",
+    url: "#",
     text: "projects",
   },
   {
     id: 4,
-    url: "/contact",
+    url: "#",
     text: "contact",
   },
   {
     id: 5,
-    url: "/profile",
+    url: "https://signal-fish.gitee.io/",
     text: "profile",
   },
 ];
